4bed02eee2cbbd3ff1796afce3089bda6c06ea21
oksaregina <122463502+oksaregina@users.noreply.github.com>
2024-07-26 17:27:32 +0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -3,10 +3,11 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize('uas_dpsi', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    dialectModule: require('mysql2')
+  });
 
 const User = require('./user')(sequelize);
 const Barang = require('./barang')(sequelize);
