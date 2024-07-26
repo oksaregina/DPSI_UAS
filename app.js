5b68a7c2b5d67f6506904bc693e9e3a57ffe743c
oksaregina <122463502+oksaregina@users.noreply.github.com>
2024-07-26 21:25:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,108 @@
-require('dotenv').config();
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var http = require('http');
+var debug = require('debug')('dpsi-uas:server');
+var dotenv = require('dotenv');
+var { sequelize } = require('./models/index');
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
 var usersRouter = require('./routes/users');
 var barangRoutes = require('./routes/barangRoutes');
 var laporanRoutes = require('./routes/laporanRoutes');
-var { sequelize } = require('./models/index');
+
+dotenv.config();
 
 var app = express();
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,11 +116,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware to handle errors
 app.use(function (err, req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-        res.status(401).json({ message: 'Token is not valid' });
-    } else {
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({ message: 'Token is not valid' });
+  } else {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 });
 
 app.use('/', indexRouter);
@@ -39,25 +130,25 @@ app.use('/barang', barangRoutes);
 app.use('/laporan', laporanRoutes);
 
 sequelize.sync().then(() => {
-    console.log('Connection has been established successfully.');
+  console.log('Connection has been established successfully.');
 }).catch((error) => {
-    console.error('Unable to connect to the database:', error);
+  console.error('Unable to connect to the database:', error);
 });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
